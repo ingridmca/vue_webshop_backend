a34d1e0cd2619d41d1a8a5ef4323bb504eb5bc80
Olga Panfilova <gr-olga@ya.ru>
2022-06-09 16:11:29 +0200

    add Review post

--- routers/reviews.js
+++ routers/reviews.js
@@ -1,4 +1,4 @@
-const { Router } = require("express");
+const {Router} = require("express");
 const Review = require("../models").review;
 const Product = require("../models").product;
 
@@ -6,10 +6,26 @@ const router = new Router();
 
 router.get("/", async (req, res) => {
     try {
-        const reviews = await Review.findAll({ include: Product });
+        const reviews = await Review.findAll({include: Product});
         res.send(reviews);
     } catch (e) {
         console.log(e.message);
+    }
+});
+
+router.post("/", async (req, res, next) => {
+    try {
+        const {name, rating, comment, productId} = req.body;
+        console.log({name, rating, comment, productId});
+        const newReview = await Review.create({
+            name: name,
+            rating: rating,
+            comment: comment,
+            productId: productId
+        },);
+        res.send(newReview);
+    } catch (e) {
+        next(e);
     }
 });
 
