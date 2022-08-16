a41387c375a4e00f2aa9e445a224d495d1b90d11
Ingrid Almeida <ingridmcalmeida@gmail.com>
2022-08-16 16:59:11 +0200

    test

--- seeders/20220609124448-reviews.js
+++ seeders/20220609124448-reviews.js
@@ -1,39 +1,40 @@
-'use strict';
+"use strict";
 
 module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.bulkInsert(
-            "reviews",
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "reviews",
 
-            [
-                {
-                    name: "Leo Messi",
-                    rating: 3,
-                    comment: "test",
-                    productId: 1,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                },
-                {
-                    name: "Leo Messi",
-                    rating: 5,
-                    comment: "test",
-                    productId: 3,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                },
-                {
-                    name: "Leo Messi",
-                    rating: 4,
-                    comment: "test",
-                    productId: 1,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                },
-            ])
-    },
+      [
+        {
+          name: "Leo Messi",
+          rating: 3,
+          comment: "test",
+          productId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Leo Messi",
+          rating: 5,
+          comment: "test",
+          productId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Leo Messii",
+          rating: 4,
+          comment: "test",
+          productId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ]
+    );
+  },
 
-    async down(queryInterface, Sequelize) {
-        await queryInterface.bulkDelete("reviews", null, {});
-    }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("reviews", null, {});
+  },
 };
